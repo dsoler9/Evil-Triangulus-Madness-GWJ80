ef62bee58f0148869ec0cba5293da8a4588c8810
Doxus <dsoler700@gmail.com>
2025-04-13 14:32:35 +0200

    Cursor and random piece system added

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,29 +1,45 @@
 extends Node
 
 @onready var mouse_cursor: Sprite2D = $MouseCursor
+#@export var piece_scene: PackedScene
+@export var piece_scenes: Array[PackedScene] = []
+@onready var pieces_node: Node2D = $"../Pieces"
 
-#var held_object = null
+var current_piece: RigidBody2D = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
 	
-	#for node in get_tree().get_nodes_in_group("pickable"):
-		#node.clicked.connect(_on_piece_clicked)
-
+	randomize()
+	spawn_piece()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	mouse_cursor.global_position = get_viewport().get_mouse_position()
 
-#func _unhandled_input(event):
-	#if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-		#if held_object and !event.pressed:
-			#held_object.drop(Input.get_last_mouse_velocity())
-			#held_object = null
-#
-#
-#func _on_piece_clicked(object):
-	#if !held_object:
-		#object.pickup()
-		#held_object = object
+func spawn_piece():
+	if piece_scenes.is_empty():
+		push_error("There is no pieces")
+		return
+	
+	var random_index = randi() % piece_scenes.size()
+	var scene = piece_scenes[random_index]
+	
+	#current_piece = piece_scene.instantiate()
+	var new_piece = scene.instantiate()
+	pieces_node.add_child(new_piece)
+	#pieces_node.add_child(current_piece)
+	
+	## TODO - Have to put it in another position
+	#current_piece.global_position = Vector2(400, 100)
+	#current_piece.freeze = true
+	new_piece.global_position = Vector2(400, 100)
+	new_piece.freeze = true
+	
+	new_piece.connect("piece_placed", Callable(self, "_on_piece_placed"))
+	current_piece = new_piece
+
+func _on_piece_placed():
+	current_piece = null
+	spawn_piece()
--- scripts/piece.gd
+++ scripts/piece.gd
@@ -1,21 +1,37 @@
 extends RigidBody2D
 
 var grabbed := false
+@onready var animations: AnimationPlayer = $AnimationPlayer
+@onready var grab_sound: AudioStreamPlayer2D = $GrabSound
+
+signal piece_placed
 
 func _input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
+		print("mouse button pressed")
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			if event.pressed:
-				grabbed = true
-				print("grab")
-				#freeze = true
-		if event.button_index == MOUSE_BUTTON_RIGHT:
-			if event.pressed:
-				print("drop")
-				grabbed = false
-				#freeze = false
+				grab()
+			else:
+				drop()
 
-func _process(delta):
+func _physics_process(delta):
 	if grabbed:
-		global_position = get_global_mouse_position()
+		global_transform.origin = get_global_mouse_position()
+	
+
+func grab():
+	#print("grab")
+	grabbed = true
+	freeze = true
+	animations.play("grabbed")
+	#grab_sound.play()
+
+func drop():
+	#print("drop")
+	grabbed = false
+	freeze = false
+	animations.stop()
+	#grab_sound.stop()
+	emit_signal("piece_placed")
 	
